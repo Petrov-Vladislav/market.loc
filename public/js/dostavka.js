da34a4a8e1aa6b0f52a90f0e245f61d17178602a
Petrov-Vladislav <pochtavladislav94@mail.ru>
2022-03-24 23:34:40 +0300

    change page delivery

--- public/js/dostavka.js
+++ public/js/dostavka.js
@@ -1,6 +1,8 @@
 let dostav_pickup = document.querySelector('#pickup');
 let dostav_delivery = document.querySelector('#delivery');
 let price_dost = document.querySelector('#price_dost');
+let button_newdostavka = document.querySelector('#button_newdostavka');
+let new_dostavka = document.querySelector('#new_dostavka');
 let sum = document.querySelector('#sum').innerHTML;
 let summa = Number(sum.slice(0, -3));
 
@@ -9,10 +11,18 @@ let total_summa = document.querySelector('#sum');
 dostav_pickup.addEventListener('click', function() {
     price_dost.innerHTML = "0 р.";
     total_summa.innerHTML = sum;
+    new_dostavka.style.cssText = 'display: none';
+    button_newdostavka.style.cssText = 'display: none';
 });
 
 dostav_delivery.addEventListener('click', function() {
     price_dost.innerHTML = "500 р.";
     total_summa.innerHTML = summa + 500 + ' р.';
+    button_newdostavka.style.cssText = 'display: block';
 });
 
+button_newdostavka.addEventListener('click', function() {
+    if(dostav_delivery.checked) {
+        new_dostavka.style.cssText = 'display: block';
+    }
+});
--- resources/views/layouts/content_cart.blade.php
+++ resources/views/layouts/content_cart.blade.php
@@ -20,7 +20,7 @@
                         
                         
                         @foreach($carts as $cart)
-                            <tr>
+                            <tr class="view_cart">
                                 <td class="cart_product_img">
                                     <img src="img/cart/{{$cart->img}}" alt="{{$cart->name}}">
                                     <form action="{{route('delFromCart')}}" method="post">
@@ -74,7 +74,59 @@
                         <li><span>Всего к оплате:</span> <span id="sum">{{$cart_sum}} р.</span></li>
                     </ul>
                     <div class="cart-btn mt-100">
-                        <a href="{{route('order')}}" class="btn amado-btn w-100">Оформить заказ</a>
+                        <button id="button_newdostavka" class="btn amado-btn w-100">Оформить доставку</button>
+                    </div>
+                </div>
+            </div>
+            <div id="new_dostavka" class="cart-table-area section-padding-100">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12 col-lg-8">
+                            <div class="checkout_details_area mt-50 clearfix">
+                                <div class="cart-title">
+                                    <h2>Оформление доставки</h2>
+                                </div>
+                                <form action="#" method="post">
+                                    <div class="row">
+                                        <div class="col-md-6 mb-3">
+                                            <input type="text" class="form-control" id="first_name" value="" placeholder="Имя" required>
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <input type="text" class="form-control" id="last_name" value="" placeholder="Фамилия" required>
+                                        </div>
+                                        <div class="col-12 mb-3">
+                                            <input type="email" class="form-control" id="email" placeholder="Email" value="" required>
+                                        </div>
+                                        <div class="col-12 mb-3">
+                                            <input id="address" name="address" type="text" class="form-control mb-3" placeholder="Адрес" value="" required>
+                                            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+                                            <link href="https://cdn.jsdelivr.net/npm/suggestions-jquery@21.12.0/dist/css/suggestions.min.css" rel="stylesheet" />
+                                            <script src="https://cdn.jsdelivr.net/npm/suggestions-jquery@21.12.0/dist/js/jquery.suggestions.min.js"></script>
+            
+                                            <script>
+                                                $("#address").suggestions({
+                                                    token: "24eb7082e0d6c2ba0a44ffdda061340d69b36dcb",
+                                                    type: "ADDRESS",
+                                                    /* Вызывается, когда пользователь выбирает одну из подсказок */
+                                                    onSelect: function(suggestion) {
+                                                        console.log(suggestion);
+                                                    }
+                                                });
+                                            </script>
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <input type="tel" class="form-control" id="phone_number" placeholder="Телефон" value="" pattern="+7[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}" required>
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <input type="date" class="form-control" id="date" placeholder="Дата доставки" value="" required>
+                                        </div>
+                                        <div class="col-12 mb-3">
+                                            <textarea name="comment" class="form-control w-100" id="comment" cols="30" rows="10" placeholder="Комментарий к заказу"></textarea>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
